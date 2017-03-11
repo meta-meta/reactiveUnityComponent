240004b831bfff6a99b1bb1cbfbd2e347fce665f
Paul M. Christian <paulmchristian@gmail.com>
2017-03-11 01:09:09 -0600

    filter functions for properties by type

---
+++ Assets/ReactiveComponent.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactiveComponent : MonoBehaviour {
+    public Dictionary<string, object> State = new Dictionary<string, object>
+    {
+        {"", 1}
+    };
+
+    private static Func<Vector3, float, Func<Vector3>> multiplyVectorScalar = (Vector3 v, float f) => () => v*f;
+    private static Func<Vector3, Vector3, Func<Vector3>> addVectors = (Vector3 v1, Vector3 v2) => () => v1 + v2;
+    private static Func<Quaternion, float, Func<Quaternion>> multiplyRotation = (Quaternion q, float f) => () => Quaternion.Euler(q.eulerAngles * f);
+    
+    public Dictionary<string, Delegate> Functions = new Dictionary<string, Delegate>
+    {
+        {"multiplyVectorScalar", multiplyVectorScalar},
+        {"addVectors", addVectors},
+        {"multiplyRotation", multiplyRotation}
+    };
+
+    public Dictionary<string, Type> PropertiesToFunctions = new Dictionary<string, Type>
+    {
+        {"localPosition", typeof(Func<Vector3>)},
+        {"localRotation", typeof(Func<Quaternion>)}
+    };
+
+    /*
+     * { position: Func<Vector3, Vector3> }
+             */
+
+    public HashSet<GameObject> gameObjects = new HashSet<GameObject>(); 
+
+    public Dictionary<string, Func<Vector3, Vector3>> transformations = new Dictionary<string, Func<Vector3, Vector3>>();
+
+    public Func<Vector3> position;
+    public Func<Quaternion> rotation;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (null != position)
+        {
+            transform.localPosition = position.Invoke();
+        }
+        if (null != rotation)
+        {
+            transform.localRotation = rotation.Invoke();
+        }
+    }
+
+
+}
+
+public static class DictionaryExcetions
+{
+    public static T Get<T>(this Dictionary<string, object> instance, string name)
+    {
+        return (T)instance[name];
+    }
+
+}
---
+++ Assets/ReactiveComponentEditor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ReactiveComponent))]
+public class ReactiveComponentEditor : Editor
+{
+    private bool btn;
+    private string gameObjectName = "";
+    private GameObject gameObject;
+
+    public override void OnInspectorGUI()
+    {
+        var component = target as ReactiveComponent;
+
+        var gameObjStyle = new GUIStyle(GUI.skin.textField);
+        gameObjStyle.fontStyle = gameObject == null ? FontStyle.Normal : FontStyle.Bold;
+        gameObjectName = GUILayout.TextField(gameObjectName, gameObjStyle);
+        gameObject = GameObject.Find(gameObjectName);
+
+        GUI.enabled = gameObject != null;
+        if (GUILayout.Button("add"))
+        {
+            component.gameObjects.Add(gameObject);
+            gameObjectName = "";
+        }
+        GUI.enabled = true;
+
+        component.gameObjects.ToList().ForEach(g =>
+        {
+            GUILayout.Label(g.name);
+            var pos = g.transform.position;
+            GUILayout.Label("Position: " + pos.x + " " + pos.y + " " + pos.z);
+            if (GUILayout.Button("doubleThis"))
+            {
+                component.position = () => g.transform.position * 2;
+            }
+
+            var rot = g.transform.rotation.eulerAngles;
+            GUILayout.Label("Rotation: " + rot.x + " " + rot.y + " " + rot.z);
+            if (GUILayout.Button("tripleTHis"))
+            {
+                component.rotation = () => Quaternion.Euler(g.transform.rotation.eulerAngles * 3);
+            }
+        });
+
+        component.PropertiesToFunctions.ToList().ForEach(pair =>
+        {
+            var propName = pair.Key;
+            var type = pair.Value;
+            GUILayout.Label(propName + " " + type.ToString());
+
+            // get the functions that have a return type matching this property's type
+            component.Functions
+                .Where((p) => p.Value.GetType().GetGenericArguments().Last() == type)
+                .ToList()
+                .ForEach(p =>
+                {
+                    GUILayout.Label(p.Key);
+                });
+        });
+
+    }
+}
